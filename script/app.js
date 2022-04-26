a5926363ff994ba58db864a65e6139800c21ce78
MohammadMahdiKaseb <mohammadmahdikaseb@gmail.com>
2022-04-26 21:02:10 +0430

    contact created

---
+++ script/app.js
@@ -0,0 +1,10 @@
+let contact = document.getElementById("modal");
+let close = document.getElementById("close");
+console.log(contact.style);
+const mdOpen = () => {
+  contact.style.display = "block";
+};
+
+const closeBtn = () => {
+  contact.style.display = "none";
+};
